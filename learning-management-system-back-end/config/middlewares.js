113d264754b510c332f83c5ddb73fae20a9fe0c6
Wasif Hasan <wasifhasancse@gmail.com>
2026-08-28 20:33:20 +0600

    config: setup cors origins

--- learning-management-system-back-end/config/middlewares.js
+++ learning-management-system-back-end/config/middlewares.js
@@ -2,7 +2,14 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:1337'],
+      headers: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
