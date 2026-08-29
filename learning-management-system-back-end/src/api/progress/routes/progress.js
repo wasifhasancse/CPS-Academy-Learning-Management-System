381d74dffbccc3db34718b9633bbaf65bf61e5be
Wasif Hasan <wasifhasancse@gmail.com>
2026-08-29 15:32:36 +0600

    routes: progress tracking

--- learning-management-system-back-end/src/api/progress/routes/progress.js
+++ learning-management-system-back-end/src/api/progress/routes/progress.js
@@ -1,5 +1,46 @@
 'use strict';
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::progress.progress');
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/progresses',
+      handler: 'api::progress.progress.find',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'GET',
+      path: '/progresses/:id',
+      handler: 'api::progress.progress.findOne',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'POST',
+      path: '/progresses',
+      handler: 'api::progress.progress.create',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'PUT',
+      path: '/progresses/:id',
+      handler: 'api::progress.progress.update',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'DELETE',
+      path: '/progresses/:id',
+      handler: 'api::progress.progress.delete',
+      config: {
+        auth: false,
+      },
+    },
+  ],
+};
