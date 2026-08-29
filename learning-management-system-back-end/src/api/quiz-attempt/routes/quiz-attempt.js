2c0293b60466b9bbcea6d0dffd1356f21727864d
Wasif Hasan <wasifhasancse@gmail.com>
2026-08-29 15:32:48 +0600

    routes: quiz attempt endpoints

--- learning-management-system-back-end/src/api/quiz-attempt/routes/quiz-attempt.js
+++ learning-management-system-back-end/src/api/quiz-attempt/routes/quiz-attempt.js
@@ -1,5 +1,46 @@
 'use strict';
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::quiz-attempt.quiz-attempt');
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/quiz-attempts',
+      handler: 'api::quiz-attempt.quiz-attempt.find',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'GET',
+      path: '/quiz-attempts/:id',
+      handler: 'api::quiz-attempt.quiz-attempt.findOne',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'POST',
+      path: '/quiz-attempts',
+      handler: 'api::quiz-attempt.quiz-attempt.create',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'PUT',
+      path: '/quiz-attempts/:id',
+      handler: 'api::quiz-attempt.quiz-attempt.update',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'DELETE',
+      path: '/quiz-attempts/:id',
+      handler: 'api::quiz-attempt.quiz-attempt.delete',
+      config: {
+        auth: false,
+      },
+    },
+  ],
+};
