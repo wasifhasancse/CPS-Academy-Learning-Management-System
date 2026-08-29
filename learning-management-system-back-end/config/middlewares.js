d453c692691eed8e370b74ea0ccd27db2321a347
Wasif Hasan <wasifhasancse@gmail.com>
2026-08-29 10:59:06 +0600

    backend: configure stripe middleware headers

--- learning-management-system-back-end/config/middlewares.js
+++ learning-management-system-back-end/config/middlewares.js
@@ -12,7 +12,12 @@ module.exports = [
   },
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      includeUnparsed: true,
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
