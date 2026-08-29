e84ec8fa1fd477e0441a93f43e3e235f147f2662
Wasif Hasan <wasifhasancse@gmail.com>
2026-08-29 15:32:05 +0600

    routes: enrollment endpoints

--- learning-management-system-back-end/src/api/enrollment/routes/enrollment.js
+++ learning-management-system-back-end/src/api/enrollment/routes/enrollment.js
@@ -1,5 +1,46 @@
 'use strict';
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::enrollment.enrollment');
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/enrollments',
+      handler: 'api::enrollment.enrollment.find',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'GET',
+      path: '/enrollments/:id',
+      handler: 'api::enrollment.enrollment.findOne',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'POST',
+      path: '/enrollments',
+      handler: 'api::enrollment.enrollment.create',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'PUT',
+      path: '/enrollments/:id',
+      handler: 'api::enrollment.enrollment.update',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'DELETE',
+      path: '/enrollments/:id',
+      handler: 'api::enrollment.enrollment.delete',
+      config: {
+        auth: false,
+      },
+    },
+  ],
+};
